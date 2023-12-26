37706351ef311dbf24d0623d8f2d16964903c053
Dwarika Kumar <dwarikabppimt202249@gmail.com>
2023-12-26 13:05:42 +0530

    experience component added

---
+++ src/Components/Works/Works.jsx
@@ -0,0 +1,25 @@
+import './Works.css'
+
+const Works = () => {
+  return (
+    <div className="works">
+        {/* left side */}
+        <div className="awesome">
+            <span>Works for All these</span>
+            <span>Brands and Clients</span>
+            <span>Transforming ideas into immersive web
+            <br />    
+             experiences, one line of code at a time, 
+            <br />    
+            to make your digital vision a reality.
+            <br />
+            Transforming ideas into immersive web
+            </span>
+            <button className="button s-button">Hire me</button>
+            <div className="blur s-blur1" style={{background: '#ABF1FF94'}}></div>
+        </div>
+    </div>
+  )
+}
+
+export default Works
